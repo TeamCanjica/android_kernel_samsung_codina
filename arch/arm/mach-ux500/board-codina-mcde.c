554232f236599d87923a9a6a0ed3f1dee3ec985d
Robert Rozic <rooobertinhooo@gmail.com>
2013-11-10 11:12:07 +0100

    Raise sleep out values

--- arch/arm/mach-ux500/board-codina-mcde.c
+++ arch/arm/mach-ux500/board-codina-mcde.c
@@ -466,7 +466,7 @@ int __init init_codina_display_devices(void)
 		codina_dpi_pri_display_info.video_mode.vsw = 2;
 		codina_dpi_pri_display_info.video_mode.vbp = 8;
 		codina_dpi_pri_display_info.video_mode.vfp = 18;
-		codina_dpi_pri_display_info.sleep_out_delay = 30;
+		codina_dpi_pri_display_info.sleep_out_delay = 50;
 	} else {
 		generic_display0.name = LCD_DRIVER_NAME_S6D27A1;
 		codina_dpi_pri_display_info.video_mode.hsw = 2;
@@ -475,7 +475,7 @@ int __init init_codina_display_devices(void)
 		codina_dpi_pri_display_info.video_mode.vsw = 2;
 		codina_dpi_pri_display_info.video_mode.vbp = 11;
 		codina_dpi_pri_display_info.video_mode.vfp = 10;
-		codina_dpi_pri_display_info.sleep_out_delay = 50;
+		codina_dpi_pri_display_info.sleep_out_delay = 100;
 	}
 	
 	ret = mcde_display_device_register(&generic_display0);
