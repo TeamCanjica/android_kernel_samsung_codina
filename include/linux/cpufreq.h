1780f84f33ba0425b202bbcf4dd8dedfb39c4f8b
judas77 <judas77@gazeta.pl>
2014-02-12 19:25:36 +0100

    add abyssplug governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -366,6 +366,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDQ)
 extern struct cpufreq_governor cpufreq_gov_ondemandq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
+extern struct cpufreq_governor cpufreq_gov_abyssplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_abyssplug)
 #endif
 
 /*********************************************************************
